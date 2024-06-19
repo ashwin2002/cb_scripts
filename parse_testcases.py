63130102999473afef35041435f09041529a06e6
Ashwin <ashwin2002@users.noreply.github.com>
2024-06-19 16:29:28 +0530

    Adding basic parse_testcase script

---
+++ parse_testcases.py
@@ -0,0 +1,134 @@
+import hashlib
+import json
+import requests
+import sys
+import xml.etree.ElementTree as EleTree
+from argparse import ArgumentParser
+from collections import OrderedDict
+from os import getenv
+
+from couchbase.exceptions import DocumentNotFoundException, \
+    PathNotFoundException
+
+from sdk_lib.sdk_conn import SDKClient
+
+
+def parse_cmd_arguments():
+    parser = ArgumentParser(description="Get test cases")
+    parser.add_argument("--server_ip", dest="gb_ip", required=True,
+                        help="Server IP on which Couchbase db is hosted")
+    parser.add_argument("--username", dest="username", default="Administrator",
+                        help="Username to use login")
+    parser.add_argument("--password", dest="password", default="password",
+                        help="Password to use login")
+    parser.add_argument("--bucket", dest="gb_bucket", default="greenboard",
+                        help="Greenboard bucket name")
+    parser.add_argument("--os", dest="os_type", default="DEBIAN",
+                        help="Target OS for which to parse the jobs")
+    parser.add_argument("--component", dest="component", required=True,
+                        help="Target component for which to parse the jobs")
+    parser.add_argument("-v", "--version", dest="version", required=True,
+                        help="Version on which the job has run")
+
+    return parser.parse_args(sys.argv[1:])
+
+
+def parse_case(case_xml):
+    data = dict()
+    for block in case_xml:
+        data[block.tag] = block.text
+    return data
+
+
+def parse_tcs(sdk_conn, cb_version, job_run_id, jenkins_build_num, xml_text):
+    root = EleTree.fromstring(xml_text)
+    for suite in root.findall("suite"):
+        for case in suite.findall("case"):
+            parsed_case = parse_case(case)
+            case_str = parsed_case["name"]
+            case_str = case_str.split(",")
+            tc = case_str[0]
+            params = dict()
+            for param in case_str[1:]:
+                key, value = param.split('=')
+                if key in ['GROUP', 'ini', 'total_testcases',
+                           'case_number', 'conf_file', 'rerun',
+                           'logs_folder', 'last_case_fail',
+                           'cluster_name', 'upgrade_version',
+                           'num_nodes', 'get-cbcollect-info', 'sirius_url']:
+                    continue
+                params[key] = value
+            params = OrderedDict(sorted(params.items()))
+            param_str = tc + json.dumps(params, separators=(',', ':'),
+                                        ensure_ascii=False)
+            param_str_bytes = param_str.encode('utf-8')
+            sha256 = hashlib.sha256()
+            sha256.update(param_str_bytes)
+            tc_hash = sha256.hexdigest()
+            try:
+                _ = sdk_conn.get_doc(tc_hash)
+            except DocumentNotFoundException:
+                print("Creating new test")
+                sdk_conn.collection.insert(tc_hash,
+                                           {"function": tc,
+                                            "params": dict(params),
+                                            "job_name": job_run_id,
+                                            "runs": {}})
+            sub_path = f"runs.`{cb_version}`"
+            # Create version field (if_req)
+            try:
+                _ = sdk_conn.get_sub_doc_as_list(tc_hash, sub_path)
+            except PathNotFoundException:
+                # Create place-holder for this version
+                print(f"New run on build: {cb_version}")
+                sdk_conn.insert_sub_doc(tc_hash, sub_path, [])
+
+            doc = sdk_conn.get_sub_doc_as_dict(tc_hash, "runs")
+            doc[cb_version].append(
+                {str(jenkins_build_num): parsed_case["status"]})
+            sdk_conn.upsert_sub_doc(tc_hash, "runs", doc)
+            exit(0)
+
+
+if __name__ == "__main__":
+    arguments = parse_cmd_arguments()
+    s3_url_prefix = "http://cb-logs-qe.s3-website-us-west-2.amazonaws.com/" \
+                    f"{arguments.version}/jenkins_logs"
+
+    test_run_info_sdk = SDKClient(arguments.gb_ip, arguments.username,
+                                  arguments.password, arguments.gb_bucket)
+    run_doc = test_run_info_sdk.get_sub_doc_as_dict(
+        f"{arguments.version}_server",
+        f"os.{arguments.os_type.upper()}.{arguments.component.upper()}")
+    test_run_info_sdk.close()
+
+    tc_db_ip = getenv("tc_db_ip")
+    tc_db_username = getenv("tc_db_username")
+    tc_db_password = getenv("tc_db_password")
+    tc_db_bucket = getenv("tc_db_bucket")
+    tc_tracker_sdk = SDKClient(tc_db_ip, tc_db_username, tc_db_password,
+                               tc_db_bucket)
+
+    for job_name, run_list in run_doc.items():
+        # # Create doc per job (if_required)
+        # try:
+        #     _ = tc_tracker_sdk.get_doc(job_name)
+        # except DocumentNotFoundException:
+        #     print(f"New job: {job_name}")
+        #     tc_tracker_sdk.collection.insert(job_name, {})
+
+        for run in run_list:
+            exec_job_type = run["url"].split("/")[-2]
+            s3_job_url = f"{s3_url_prefix}/{exec_job_type}/{run['build_id']}/"
+            test_result_url = s3_job_url + "testresult.xml"
+            print(test_result_url)
+            xml_text_data = requests.get(test_result_url).text
+            if "404 Not Found" in xml_text_data:
+                print(f"Job: {job_name}, testresult.xml not found")
+                continue
+            parse_tcs(tc_tracker_sdk, arguments.version,
+                      job_name, run['build_id'], xml_text_data)
+
+            # if str(run["url"]).endswith("/test_suite_executor-TAF/"):
+
+    tc_tracker_sdk.close()
